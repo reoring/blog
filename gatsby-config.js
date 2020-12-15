fbfa04dc4330aec9bee5a15a287be16f1ed564a6
reoring <reoring@craftsman-software.com>
2020-12-15 10:13:43 +0900

    docs: change metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `reoring blog`,
+        short_name: `Reoring`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
